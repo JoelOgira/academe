538b11de9164a4494862fd8f07f833fe934b8ab3
Jay <joelogira@gmail.com>
2024-09-29 17:34:40 +0300

    Created assignments list page

---
+++ src/app/(dashboard)/list/assignments/page.tsx
@@ -0,0 +1,104 @@
+import Pagination from "@/components/pagination";
+import { SearchInput } from "@/components/search-input";
+import Table from "@/components/table";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { assignmentsData, role } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type Assignment = {
+  id: number;
+  subject: string;
+  class: string;
+  teacher: string;
+  dueDate: string;
+};
+
+const columns = [
+  {
+    header: "Subject Name",
+    accessor: "subjectName",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Due Date",
+    accessor: "dueDate",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+export default function AssignmentListPage() {
+  const renderRow = (item: Assignment) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
+    >
+      <td className="p-4">
+        <h3 className="font-semibold">{item.subject}</h3>
+      </td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.dueDate}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/assignments/${item.id}`}>
+            <button className="size-7 flex items-center justify-center rounded-full bg-skyBlue">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="size-7 flex items-center justify-center rounded-full bg-skyPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <Card className="flex flex-1 gap-4 flex-col border-none">
+      <CardHeader className="p-4 flex-row items-center justify-between space-y-0 md:space-x-6">
+        <CardTitle className="hidden md:text-xl md:block">
+          All Assignments
+        </CardTitle>
+        <div className="w-full flex flex-col justify-center items-center gap-4 md:flex-row md:w-auto ">
+          <SearchInput />
+          <div className="flex items-center gap-4 justify-end">
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image
+                width={14}
+                height={14}
+                alt="filter icon"
+                src="/filter.png"
+              />
+            </button>
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image width={14} height={14} alt="sort icon" src="/sort.png" />
+            </button>
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image width={14} height={14} alt="plus icon" src="/plus.png" />
+            </button>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="p-4">
+        <Table columns={columns} renderRow={renderRow} data={assignmentsData} />
+      </CardContent>
+      <Pagination />
+    </Card>
+  );
+}
