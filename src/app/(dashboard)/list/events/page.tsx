0f83664ca70823f19a2066b149243487d03ff92c
Jay <joelogira@gmail.com>
2024-09-29 17:35:48 +0300

    Created events list page

---
+++ src/app/(dashboard)/list/events/page.tsx
@@ -0,0 +1,109 @@
+import Pagination from "@/components/pagination";
+import { SearchInput } from "@/components/search-input";
+import Table from "@/components/table";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { eventsData, role } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+
+type Event = {
+  id: number;
+  title: string;
+  class: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+};
+
+const columns = [
+  {
+    header: "Event Title",
+    accessor: "eventTitle",
+  },
+  {
+    header: "Class",
+    accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Date",
+    accessor: "date",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Start Time",
+    accessor: "startTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "End Time",
+    accessor: "endTime",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Actions",
+    accessor: "actions",
+  },
+];
+
+export default function EventListPage() {
+  const renderRow = (item: Event) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
+    >
+      <td className="p-4">
+        <h3 className="font-semibold">{item.title}</h3>
+      </td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
+      <td className="hidden md:table-cell">{item.startTime}</td>
+      <td className="hidden md:table-cell">{item.endTime}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          <Link href={`/list/events/${item.id}`}>
+            <button className="size-7 flex items-center justify-center rounded-full bg-skyBlue">
+              <Image src="/view.png" alt="" width={16} height={16} />
+            </button>
+          </Link>
+          {role === "admin" && (
+            <button className="size-7 flex items-center justify-center rounded-full bg-skyPurple">
+              <Image src="/delete.png" alt="" width={16} height={16} />
+            </button>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
+
+  return (
+    <Card className="flex flex-1 gap-4 flex-col border-none">
+      <CardHeader className="p-4 flex-row items-center justify-between space-y-0 md:space-x-6">
+        <CardTitle className="hidden md:text-xl md:block">All Events</CardTitle>
+        <div className="w-full flex flex-col justify-center items-center gap-4 md:flex-row md:w-auto ">
+          <SearchInput />
+          <div className="flex items-center gap-4 justify-end">
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image
+                width={14}
+                height={14}
+                alt="filter icon"
+                src="/filter.png"
+              />
+            </button>
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image width={14} height={14} alt="sort icon" src="/sort.png" />
+            </button>
+            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
+              <Image width={14} height={14} alt="plus icon" src="/plus.png" />
+            </button>
+          </div>
+        </div>
+      </CardHeader>
+      <CardContent className="p-4">
+        <Table columns={columns} renderRow={renderRow} data={eventsData} />
+      </CardContent>
+      <Pagination />
+    </Card>
+  );
+}
