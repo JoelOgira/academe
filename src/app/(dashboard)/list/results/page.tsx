c689d2a74d7006938bbee2568e0dbd5faac44ff7
Jay <joelogira@gmail.com>
2024-10-24 14:57:11 +0300

    Fetched results data from the database

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -1,30 +1,40 @@
-import Pagination from "@/components/pagination";
+import React from "react";
+import FormModal from "@/components/form-modal";
 import { SearchInput } from "@/components/search-input";
 import Table from "@/components/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { resultsData, role } from "@/lib/data";
+import { currentUserId, role } from "@/lib/settings";
 import Image from "next/image";
-import Link from "next/link";
+import { Prisma } from "@prisma/client";
+import prisma from "@/lib/prisma";
+import { formatDate, ITEM_PER_PAGE } from "@/lib/utils";
 
-type Result = {
+import TablePagination from "@/components/table-pagination";
+type ResultsList = {
   id: number;
-  subject: string;
-  class: string;
-  teacher: string;
-  student: string;
-  date: string;
-  type: "exam" | "assignment";
+  title: string;
+  studentName: string;
+  studentSurname: string;
+  teacherName: string;
+  teacherSurname: string;
   score: number;
+  className: string;
+  startTime: Date;
 };
 
 const columns = [
   {
-    header: "Subject Name",
-    accessor: "subjectName",
+    header: "Title",
+    accessor: "title",
   },
   {
     header: "Class",
     accessor: "class",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Student",
+    accessor: "student",
     className: "hidden md:table-cell",
   },
   {
@@ -33,62 +43,163 @@ const columns = [
     className: "hidden lg:table-cell",
   },
   {
-    header: "Student",
-    accessor: "student",
-    className: "hidden md:table-cell",
-  },
-  {
     header: "Date",
     accessor: "date",
     className: "hidden lg:table-cell",
-  },
-  {
-    header: "Result Type",
-    accessor: "type",
-    className: "hidden md:table-cell",
   },
   {
     header: "Score",
     accessor: "score",
     className: "hidden md:table-cell",
   },
-  {
-    header: "Actions",
-    accessor: "actions",
-  },
+  ...(role === "admin" || role === "teacher"
+    ? [
+        {
+          header: "Actions",
+          accessor: "actions",
+        },
+      ]
+    : []),
 ];
 
-export default function ResultListPage() {
-  const renderRow = (item: Result) => (
-    <tr
-      key={item.id}
-      className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
-    >
-      <td className="p-4">
-        <h3 className="font-semibold">{item.subject}</h3>
-      </td>
-      <td className="hidden md:table-cell">{item.class}</td>
-      <td className="hidden lg:table-cell">{item.teacher}</td>
-      <td className="hidden md:table-cell">{item.student}</td>
-      <td className="hidden lg:table-cell">{item.date}</td>
-      <td className="hidden md:table-cell">{item.type}</td>
-      <td className="hidden md:table-cell">{item.score}</td>
-      <td>
-        <div className="flex items-center gap-2">
-          <Link href={`/list/results/${item.id}`}>
-            <button className="size-7 flex items-center justify-center rounded-full bg-skyBlue">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
-          {role === "admin" && (
-            <button className="size-7 flex items-center justify-center rounded-full bg-skyPurple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
-            </button>
-          )}
-        </div>
-      </td>
-    </tr>
-  );
+const renderRow = (item: ResultsList) => (
+  <tr
+    key={item.id}
+    className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
+  >
+    <td className="p-4">
+      <h3 className="font-semibold">{item.title}</h3>
+    </td>
+    <td className="hidden md:table-cell">{item.className}</td>
+    <td className="hidden lg:table-cell">{`${item.studentName} ${item.studentSurname}`}</td>
+    <td className="hidden md:table-cell">{`${item.teacherName} ${item.teacherSurname}`}</td>
+    <td className="hidden lg:table-cell">{formatDate(item.startTime)}</td>
+    <td className="hidden md:table-cell">{item.score}</td>
+    <td>
+      <div className="flex items-center gap-2">
+        {(role === "admin" || role === "teacher") && (
+          <>
+            <FormModal table="result" type="update" data={item} />
+            <FormModal table="result" type="delete" id={item.id} />
+          </>
+        )}
+      </div>
+    </td>
+  </tr>
+);
+
+export default async function ResultListPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
+  const { page, ...queryParams } = searchParams;
+
+  const p = page ? parseInt(page) : 1;
+
+  const query: Prisma.ResultWhereInput = {};
+
+  if (queryParams) {
+    for (const [key, value] of Object.entries(queryParams)) {
+      if (value !== undefined) {
+        switch (key) {
+          case "studentId":
+            query.studentId = value;
+          case "search":
+            query.OR = [
+              { exam: { title: { contains: value, mode: "insensitive" } } },
+              {
+                student: {
+                  OR: [
+                    { name: { contains: value, mode: "insensitive" } },
+                    { surname: { contains: value, mode: "insensitive" } },
+                  ],
+                },
+              },
+            ];
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+  }
+
+  // ROLE CONDITIONS
+  switch (role) {
+    case "admin":
+      break;
+    case "teacher":
+      query.OR = [
+        { exam: { lesson: { teacherId: currentUserId! } } },
+        { assignment: { lesson: { teacherId: currentUserId! } } },
+      ];
+      break;
+    case "student":
+      query.studentId = currentUserId!;
+      break;
+    case "parent":
+      query.student = {
+        parentId: currentUserId!,
+      };
+    default:
+      break;
+  }
+
+  const [dataRes, count] = await prisma.$transaction([
+    prisma.result.findMany({
+      where: query,
+      take: ITEM_PER_PAGE,
+      skip: ITEM_PER_PAGE * (p - 1),
+      include: {
+        student: {
+          select: { name: true, surname: true },
+        },
+        exam: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true, surname: true } },
+              },
+            },
+          },
+        },
+        assignment: {
+          include: {
+            lesson: {
+              select: {
+                class: { select: { name: true } },
+                teacher: { select: { name: true, surname: true } },
+              },
+            },
+          },
+        },
+      },
+    }),
+    prisma.result.count({ where: query }),
+  ]);
+
+  const data = dataRes.map((item) => {
+    const assessment = item.exam || item.assignment;
+
+    if (!assessment) return null;
+
+    const isExam = "startTime" in assessment;
+
+    return {
+      id: item.id,
+      title: assessment.title,
+      studentName: item.student.name,
+      studentSurname: item.student.surname,
+      teacherName: assessment.lesson.teacher.name,
+      teacherSurname: assessment.lesson.teacher.surname,
+      score: item.score,
+      className: assessment.lesson.class.name,
+      startTime: isExam ? assessment.startTime : assessment.startDate,
+    };
+  });
 
   return (
     <Card className="flex flex-1 gap-4 flex-col border-none">
@@ -110,16 +221,14 @@ export default function ResultListPage() {
             <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
               <Image width={14} height={14} alt="sort icon" src="/sort.png" />
             </button>
-            <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
-              <Image width={14} height={14} alt="plus icon" src="/plus.png" />
-            </button>
+            {role === "admin" && <FormModal table="assignment" type="create" />}
           </div>
         </div>
       </CardHeader>
       <CardContent className="p-4">
-        <Table columns={columns} renderRow={renderRow} data={resultsData} />
+        <Table columns={columns} renderRow={renderRow} data={data} />
       </CardContent>
-      <Pagination />
+      <TablePagination page={p} count={count} />
     </Card>
   );
 }
