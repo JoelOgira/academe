111271da04e6d38ce5f034af700338dbffc29b7b
Jay <joelogira@gmail.com>
2024-11-22 11:50:33 +0300

    Added form-container

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import FormModal from "@/components/form-modal";
 import { SearchInput } from "@/components/search-input";
 import Table from "@/components/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
@@ -7,9 +5,10 @@ import { role } from "@/lib/settings";
 import Image from "next/image";
 import { Prisma, Subject, Teacher } from "@prisma/client";
 import prisma from "@/lib/prisma";
-import { ITEM_PER_PAGE } from "@/lib/utils";
+import { cn, ITEM_PER_PAGE } from "@/lib/utils";
 import TablePagination from "@/components/table-pagination";
 import { Metadata } from "next";
+import FormContainer from "@/components/form-container";
 
 export const metadata: Metadata = {
   title: "subjects list",
@@ -33,36 +32,14 @@ const columns = [
   },
 ];
 
-const renderRow = (item: SubjectList) => (
-  <tr
-    key={item.id}
-    className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
-  >
-    <td className="p-4">
-      <h3 className="font-semibold">{item.name}</h3>
-    </td>
-    
-    <td className="hidden md:table-cell">
-      {item.teachers.map((teacher) => teacher.name).join(", ")}
-    </td>
-
-    <td>
-      <div className="flex items-center gap-2">
-        <FormModal table="subject" type="update" data={item} />
-        <FormModal table="subject" type="delete" id={item.id} />
-      </div>
-    </td>
-  </tr>
-);
-
 export default async function SubjectListPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
-  const { page, ...queryParams } = searchParams;
+  const { page, highlight, ...queryParams } = searchParams;
 
-  const p = page ? parseInt(page) : 1;
+  const p = queryParams.search ? 1 : page ? parseInt(page) : 1;
 
   const query: Prisma.SubjectWhereInput = {};
 
@@ -93,9 +70,38 @@ export default async function SubjectListPage({
       },
       take: ITEM_PER_PAGE,
       skip: ITEM_PER_PAGE * (p - 1),
+      orderBy: {
+        name: "asc",
+      },
     }),
     prisma.subject.count({ where: query }),
   ]);
+
+  const renderRow = (item: SubjectList) => (
+    <tr
+      key={item.id}
+      className={cn(
+        "text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple",
+        highlight === item.id.toString() &&
+          "bg-lightSkyYellow transition-colors duration 300"
+      )}
+    >
+      <td className="p-4">
+        <h3 className="font-semibold capitalize">{item.name}</h3>
+      </td>
+
+      <td className="hidden md:table-cell">
+        {item.teachers.map((teacher) => teacher.name).join(", ")}
+      </td>
+
+      <td>
+        <div className="flex items-center gap-2">
+          <FormContainer table="subject" type="update" data={item} />
+          <FormContainer table="subject" type="delete" id={item.id} />
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <Card className="flex flex-1 gap-4 flex-col border-none">
@@ -117,7 +123,9 @@ export default async function SubjectListPage({
             <button className="size-8 bg-skyYellow rounded-full flex justify-center items-center">
               <Image width={14} height={14} alt="sort icon" src="/sort.png" />
             </button>
-            {role === "admin" && <FormModal table="subject" type="create" />}
+            {role === "admin" && (
+              <FormContainer table="subject" type="create" />
+            )}
           </div>
         </div>
       </CardHeader>
