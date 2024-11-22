272a861ac2011a2bb14523e6c5ada3b899048022
Jay <joelogira@gmail.com>
2024-11-22 12:07:13 +0300

    Added subject validation schema

--- src/lib/form-validation-schema.ts
+++ src/lib/form-validation-schema.ts
@@ -1,9 +1,20 @@
 import { z } from "zod";
 
 export const subjectSchema = z.object({
+  id: z.coerce.number().optional(),
   name: z.string().min(1, {
     message: "subject name is required.",
   }),
+  teachers: z
+    .array(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+      })
+    )
+    .min(1, {
+      message: "At least one teacher is required",
+    }),
 });
 
 export type SubjectSchema = z.infer<typeof subjectSchema>;
