261ec703153c5c653a932dc0384db40fd7b95d5b
Jay <joelogira@gmail.com>
2024-11-28 07:09:42 +0300

    Fetched class related data

--- src/components/form-container.tsx
+++ src/components/form-container.tsx
@@ -43,6 +43,17 @@ export default async function FormContainer({
         relatedData = { teachers: subjectTeachers };
         break;
 
+      case "class":
+        const classGrades = await prisma.grade.findMany({
+          select: { id: true, level: true },
+        });
+        const classTeachers = await prisma.teacher.findMany({
+          select: { id: true, name: true, surname: true },
+        });
+
+        relatedData = { grades: classGrades, teachers: classTeachers };
+        break;
+
       default:
         break;
     }
