9dd832524e3a749d993669b539ab9a4faa030522
Jay <joelogira@gmail.com>
2024-11-28 07:11:41 +0300

    Updated the validation schema

--- src/lib/form-validation-schema.ts
+++ src/lib/form-validation-schema.ts
@@ -20,15 +20,16 @@ export type SubjectSchema = z.infer<typeof subjectSchema>;
 
 export const classSchema = z.object({
   id: z.coerce.number().optional(),
-  name: z.string().min(1, {
-    message: "Class name is required",
-  }),
-  capacity: z.coerce.number().min(1, {
-    message: "Class capacity is required",
-  }),
-  grade: z.number().min(1, {
-    message: "Grade is required",
-  }),
-  supervisorId: z.string().optional(),
+  name: z.string().min(1, { message: "Subject name is required" }),
+  capacity: z.coerce
+    .number({
+      invalid_type_error: "Capacity must be a number and is required",
+    })
+    .positive({ message: "Capacity must be greater than 0" })
+    .min(1, { message: "Class capacity is required!" }),
+  gradeId: z.coerce
+    .number({ invalid_type_error: "Grade is required" })
+    .min(1, { message: "Grade name is required!" }),
+  supervisorId: z.coerce.string().optional(),
 });
 export type ClassSchema = z.infer<typeof classSchema>;
