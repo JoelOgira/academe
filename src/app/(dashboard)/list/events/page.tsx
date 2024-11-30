467d181aedc805cd5ba2fe8a2dcb5990d8dd632b
Jay <joelogira@gmail.com>
2024-11-30 22:35:50 +0300

    Updated auth and role import

--- src/app/(dashboard)/list/events/page.tsx
+++ src/app/(dashboard)/list/events/page.tsx
@@ -8,9 +8,8 @@ import { Class, Event, Prisma } from "@prisma/client";
 import prisma from "@/lib/prisma";
 import { formatDate, ITEM_PER_PAGE } from "@/lib/utils";
 import TablePagination from "@/components/table-pagination";
-import { role } from "@/lib/settings";
-import { currentUserId } from "@/lib/settings";
 import { Metadata } from "next";
+import { auth } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: "events list",
@@ -18,83 +17,15 @@ export const metadata: Metadata = {
 
 type EventsList = Event & { class: Class };
 
-const columns = [
-  {
-    header: "Event Title",
-    accessor: "eventTitle",
-  },
-  {
-    header: "Class",
-    accessor: "class",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Date",
-    accessor: "date",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Start Time",
-    accessor: "startTime",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "End Time",
-    accessor: "endTime",
-    className: "hidden md:table-cell",
-  },
-  ...(role === "Admin" || role === "Teacher"
-    ? [
-        {
-          header: "Actions",
-          accessor: "actions",
-        },
-      ]
-    : []),
-];
-
-const renderRow = (item: EventsList) => (
-  <tr
-    key={item.id}
-    className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
-  >
-    <td className="p-4">
-      <h3 className="font-semibold">{item.title}</h3>
-    </td>
-    <td className="hidden md:table-cell">{item.class?.name || "-"}</td>
-    <td className="hidden md:table-cell">{formatDate(item.startTime)}</td>
-    <td className="hidden md:table-cell">
-      {item.startTime.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
-    </td>
-    <td className="hidden md:table-cell">
-      {item.endTime.toLocaleTimeString("en-US", {
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: false,
-      })}
-    </td>
-    <td>
-      <div className="flex items-center gap-2">
-        {role === "admin" && (
-          <>
-            <FormModal table="event" type="update" data={item} />
-            <FormModal table="event" type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
-
 export default async function EventListPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
+  const { sessionClaims, userId } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
   const { page, ...queryParams } = searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -177,6 +108,78 @@ export default async function EventListPage({
     }),
     prisma.event.count({ where: query }),
   ]);
+
+  const columns = [
+    {
+      header: "Event Title",
+      accessor: "eventTitle",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Date",
+      accessor: "date",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Start Time",
+      accessor: "startTime",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "End Time",
+      accessor: "endTime",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "Admin" || role === "Teacher"
+      ? [
+          {
+            header: "Actions",
+            accessor: "actions",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: EventsList) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
+    >
+      <td className="p-4">
+        <h3 className="font-semibold">{item.title}</h3>
+      </td>
+      <td className="hidden md:table-cell">{item.class?.name || "-"}</td>
+      <td className="hidden md:table-cell">{formatDate(item.startTime)}</td>
+      <td className="hidden md:table-cell">
+        {item.startTime.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </td>
+      <td className="hidden md:table-cell">
+        {item.endTime.toLocaleTimeString("en-US", {
+          hour: "2-digit",
+          minute: "2-digit",
+          hour12: false,
+        })}
+      </td>
+      <td>
+        <div className="flex items-center gap-2">
+          {role === "admin" && (
+            <>
+              <FormModal table="event" type="update" data={item} />
+              <FormModal table="event" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <Card className="flex flex-1 gap-4 flex-col border-none">
