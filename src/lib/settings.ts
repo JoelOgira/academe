2da75e4670c71cfda00d623f57429a58c69e8d03
Jay <joelogira@gmail.com>
2024-11-30 22:33:38 +0300

    Deleted settings component

--- src/lib/settings.ts
+++
@@ -1,5 +0,0 @@
-import { auth } from "@clerk/nextjs/server";
-
-const { sessionClaims, userId } = auth();
-export const role = (sessionClaims?.metadata as { role?: string })?.role;
-export const currentUserId = userId;
