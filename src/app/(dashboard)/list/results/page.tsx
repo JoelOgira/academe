b5f17e7c8ca8692e67bbe2691a6ee2cbcb1bec17
Jay <joelogira@gmail.com>
2024-12-05 22:17:11 +0300

    Imported roles and user id directly into the page

--- src/app/(dashboard)/list/results/page.tsx
+++ src/app/(dashboard)/list/results/page.tsx
@@ -3,13 +3,13 @@ import FormModal from "@/components/form-modal";
 import { SearchInput } from "@/components/search-input";
 import Table from "@/components/table";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { currentUserId, role } from "@/lib/settings";
 import Image from "next/image";
 import { Prisma } from "@prisma/client";
 import prisma from "@/lib/prisma";
 import { formatDate, ITEM_PER_PAGE } from "@/lib/utils";
 import TablePagination from "@/components/table-pagination";
 import { Metadata } from "next";
+import { auth } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: "results list",
@@ -27,77 +27,15 @@ type ResultsList = {
   startTime: Date;
 };
 
-const columns = [
-  {
-    header: "Title",
-    accessor: "title",
-  },
-  {
-    header: "Class",
-    accessor: "class",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Student",
-    accessor: "student",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Teacher",
-    accessor: "teacher",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Date",
-    accessor: "date",
-    className: "hidden lg:table-cell",
-  },
-  {
-    header: "Score",
-    accessor: "score",
-    className: "hidden md:table-cell",
-  },
-  ...(role === "admin" || role === "teacher"
-    ? [
-        {
-          header: "Actions",
-          accessor: "actions",
-        },
-      ]
-    : []),
-];
-
-const renderRow = (item: ResultsList) => (
-  <tr
-    key={item.id}
-    className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
-  >
-    <td className="p-4">
-      <h3 className="font-semibold">{item.title}</h3>
-    </td>
-    <td className="hidden md:table-cell">{item.className}</td>
-    <td className="hidden lg:table-cell">{`${item.studentName} ${item.studentSurname}`}</td>
-    <td className="hidden md:table-cell">{`${item.teacherName} ${item.teacherSurname}`}</td>
-    <td className="hidden lg:table-cell">{formatDate(item.startTime)}</td>
-    <td className="hidden md:table-cell">{item.score}</td>
-    <td>
-      <div className="flex items-center gap-2">
-        {(role === "admin" || role === "teacher") && (
-          <>
-            <FormModal table="result" type="update" data={item} />
-            <FormModal table="result" type="delete" id={item.id} />
-          </>
-        )}
-      </div>
-    </td>
-  </tr>
-);
-
 export default async function ResultListPage({
   searchParams,
 }: {
   searchParams: { [key: string]: string | undefined };
 }) {
+  const { sessionClaims, userId } = auth();
+  const role = (sessionClaims?.metadata as { role?: string })?.role;
+  const currentUserId = userId;
+
   const { page, ...queryParams } = searchParams;
 
   const p = page ? parseInt(page) : 1;
@@ -205,6 +143,72 @@ export default async function ResultListPage({
       startTime: isExam ? assessment.startTime : assessment.startDate,
     };
   });
+
+  const columns = [
+    {
+      header: "Title",
+      accessor: "title",
+    },
+    {
+      header: "Class",
+      accessor: "class",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Student",
+      accessor: "student",
+      className: "hidden md:table-cell",
+    },
+    {
+      header: "Teacher",
+      accessor: "teacher",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Date",
+      accessor: "date",
+      className: "hidden lg:table-cell",
+    },
+    {
+      header: "Score",
+      accessor: "score",
+      className: "hidden md:table-cell",
+    },
+    ...(role === "admin" || role === "teacher"
+      ? [
+          {
+            header: "Actions",
+            accessor: "actions",
+          },
+        ]
+      : []),
+  ];
+
+  const renderRow = (item: ResultsList) => (
+    <tr
+      key={item.id}
+      className="text-sm border-b border-gray-200 even:bg-gray-50 hover:bg-lightSkyPurple"
+    >
+      <td className="p-4">
+        <h3 className="font-semibold">{item.title}</h3>
+      </td>
+      <td className="hidden md:table-cell">{item.className}</td>
+      <td className="hidden lg:table-cell">{`${item.studentName} ${item.studentSurname}`}</td>
+      <td className="hidden md:table-cell">{`${item.teacherName} ${item.teacherSurname}`}</td>
+      <td className="hidden lg:table-cell">{formatDate(item.startTime)}</td>
+      <td className="hidden md:table-cell">{item.score}</td>
+      <td>
+        <div className="flex items-center gap-2">
+          {(role === "admin" || role === "teacher") && (
+            <>
+              <FormModal table="result" type="update" data={item} />
+              <FormModal table="result" type="delete" id={item.id} />
+            </>
+          )}
+        </div>
+      </td>
+    </tr>
+  );
 
   return (
     <Card className="flex flex-1 gap-4 flex-col border-none">
