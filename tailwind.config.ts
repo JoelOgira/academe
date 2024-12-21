75932bcea22ca1a1bb995f74f01b17666ad5de9d
Jay <joelogira@gmail.com>
2024-12-21 19:02:01 +0300

    Added styling for uploadthing

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config: Config = {
+export const config: Config = withUt({
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -71,5 +72,5 @@ const config: Config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-};
+});
 export default config;
