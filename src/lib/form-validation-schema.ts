4ce0da7f1f286b92ec1a56bee3b8d0c5eaba67ef
Jay <joelogira@gmail.com>
2024-12-21 18:47:40 +0300

    Added teacher schema

--- src/lib/form-validation-schema.ts
+++ src/lib/form-validation-schema.ts
@@ -5,16 +5,9 @@ export const subjectSchema = z.object({
   name: z.string().min(1, {
     message: "subject name is required.",
   }),
-  teachers: z
-    .array(
-      z.object({
-        id: z.string(),
-        name: z.string(),
-      })
-    )
-    .min(1, {
-      message: "At least one teacher is required",
-    }),
+  teachers: z.array(z.string()).min(1, {
+    message: "At least one teacher is required",
+  }),
 });
 export type SubjectSchema = z.infer<typeof subjectSchema>;
 
@@ -33,3 +26,43 @@ export const classSchema = z.object({
   supervisorId: z.coerce.string().optional(),
 });
 export type ClassSchema = z.infer<typeof classSchema>;
+
+export const teacherSchema = z.object({
+  id: z.string().optional(),
+  username: z
+    .string()
+    .min(3, {
+      message: "Username must be at least 3 characters.",
+    })
+    .max(20, {
+      message: "Username must be at most 20 characters.",
+    }),
+  password: z
+    .string()
+    .min(8, {
+      message: "Password must be at least 8 characters.",
+    })
+    .optional()
+    .or(z.literal("")),
+  name: z.string().min(3, {
+    message: "First name must be at least 3 characters.",
+  }),
+  surname: z.string().min(3, {
+    message: "Surname must be at least 3 characters.",
+  }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address." })
+    .optional()
+    .or(z.literal("")),
+  phone: z.string().optional(),
+  address: z.string().optional(),
+  img: z.string().optional(),
+  bloodType: z.string().optional(),
+  birthday: z.string().refine((date) => !isNaN(Date.parse(date)), {
+    message: "Invalid date format",
+  }),
+  sex: z.enum(["MALE", "FEMALE"], { message: "Gender is required" }),
+  subjects: z.array(z.string()).optional(),
+});
+export type TeacherSchema = z.infer<typeof teacherSchema>;
