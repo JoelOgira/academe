84c94dc9f8812ea21cbd749e1ccdccb461ab5cf0
Jay <joelogira@gmail.com>
2025-01-04 20:54:38 +0300

    Added endpoint for student image

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -14,6 +14,9 @@ export const ourFileRouter = {
   teacherImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
     .onUploadComplete(() => {}),
+  studentImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
